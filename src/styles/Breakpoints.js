ff3374e810ab14a4ae438eca9f8876d4f20aa814
josedoce <josesamya@gmail.com>
2021-02-04 12:46:34 -0300

    estilo breakpoints

---
+++ src/styles/Breakpoints.js
@@ -0,0 +1,10 @@
+export const BreakpointSize = {
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+};
+
+export const BreakAt = size => `@media(min-width: ${size}px)`;
+
+//aqui estão definidos os padrões de tela
