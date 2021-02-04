d14b8f879143e9764bfb557df963e053ecdd47f6
josedoce <josesamya@gmail.com>
2021-02-04 12:47:28 -0300

    estilo provider

---
+++ src/styles/ThemeProvider.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {amarelo} from './Cor';
+import {ThemeProvider as StyledProvider} from 'styled-components';
+//sera usado como global
+
+//usando theme no storybook
+
+export const ThemeNames = {
+  light: 'light',
+  ocean: 'ocean',
+}
+const light = { //definindo uma variação de cores
+  colors: {
+    primary: {
+      main: amarelo,
+      dark: '#c79100',
+      light: '#fff350',
+      text: '#212121'
+    },
+    danger: {
+      main: '#ff3d00',
+      dark: '#b22a00',
+      text: '#212121',
+    }
+  }
+}
+
+const allThemes = {
+  light,
+  ocean: {
+    ...light,
+    colors: {
+      ...light.colors,
+      primary: {
+        main: '#2196f3',
+        dark: '#1769aa',
+        light: '#4dabf5',
+        text: '#fff'
+      }
+    }
+  }
+}
+
+const ThemeProvider = ( {theme, children} ) => (
+  <StyledProvider theme={allThemes[theme]}>
+    {children}
+  </StyledProvider>
+);
+
+ThemeProvider.defaultProps = {
+  theme: 'light'
+}
+
+export default ThemeProvider;
