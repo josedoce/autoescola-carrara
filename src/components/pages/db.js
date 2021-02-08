7405457ff81eed44cd639d6e19918e1b89ea5304
josedoce <josesamya@gmail.com>
2021-02-07 21:43:09 -0300

    produtos para o products grid card

---
+++ src/components/pages/db.js
@@ -0,0 +1,32 @@
+export const produto = [
+    {
+        id: 0,
+        imagem: "https://st2.depositphotos.com/1038919/8555/i/600/depositphotos_85553004-stock-photo-woman-driving-a-car.jpg",
+        titulo: "titulo qualquer 1",
+        texto: "Lorem ipsum dolor sit amed, consectetur adipiscing elit, sed do eiusmod tempor",
+    },
+    {
+        id: 1,
+        imagem: "https://st4.depositphotos.com/3542901/20589/i/600/depositphotos_205897738-stock-photo-vintage-tone-image-people-driving.jpg",
+        titulo: "titulo qualquer 2",
+        texto: "Lorem ipsum dolor sit amed, consectetur adipiscing elit, sed do eiusmod tempor",
+    },
+    {
+        id: 2,
+        imagem: "https://st2.depositphotos.com/1038919/8555/i/600/depositphotos_85553004-stock-photo-woman-driving-a-car.jpg",
+        titulo: "titulo qualquer 3",
+        texto: "Lorem ipsum dolor sit amed, consectetur adipiscing elit, sed do eiusmod tempor",
+    },
+    {
+        id: 3,
+        imagem: "https://st4.depositphotos.com/3542901/20589/i/600/depositphotos_205897738-stock-photo-vintage-tone-image-people-driving.jpg",
+        titulo: "titulo qualquer 4",
+        texto: "Lorem ipsum dolor sit amed, consectetur adipiscing elit, sed do eiusmod tempor",
+    },
+    {
+        id: 4,
+        imagem: "https://st2.depositphotos.com/1038919/8555/i/600/depositphotos_85553004-stock-photo-woman-driving-a-car.jpg",
+        titulo: "titulo qualquer 5",
+        texto: "Lorem ipsum dolor sit amed, consectetur adipiscing elit, sed do eiusmod tempor",
+    },
+];
